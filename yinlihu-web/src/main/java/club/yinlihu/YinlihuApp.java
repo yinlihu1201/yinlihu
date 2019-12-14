89f0e74a447d4b0105b2c100f1b6e5273d63aae3
yinlihu <yinlihu1201@outlook.com>
2019-12-14 12:19:04 +0800

    springboot支持maven多环境打包，指定配置文件，并支持war包部署

--- yinlihu-web/src/main/java/club/yinlihu/YinlihuApp.java
+++ yinlihu-web/src/main/java/club/yinlihu/YinlihuApp.java
@@ -5,10 +5,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@MapperScan("club.yinlihu.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,FreeMarkerAutoConfiguration.class})
-public class YinlihuApp {
+public class YinlihuApp extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(YinlihuApp.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(YinlihuApp.class, args);
 	}
